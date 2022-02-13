c7bd5e9a4c4ca6373ea318d8198c78a471de0b08
Johnson Chua <chua.johnson@gmail.com>
2022-02-14 07:56:03 +0800

    added adapter interface and class

---
+++ VoyagerProducts/ClientLibrary/PointOfSaleTerminal.cs
@@ -0,0 +1,18 @@
+﻿namespace VoyagerProducts.ClientLibrary
+{
+    using VoyagerProducts.Controllers;
+    using VoyagerProducts.Interfacce;
+
+    public class PointOfSaleTerminal : IPos
+    {
+        private readonly ProductController _prodController;
+        public PointOfSaleTerminal(ProductController prodController) => this._prodController = prodController;
+
+        public decimal CalculateTotal() => _prodController.ComputeTotal();
+
+        public void ScanProduct(string prodCode) => _prodController.AddProduct(prodCode);
+
+        public void SetPricing() => _prodController.SetPrice();
+
+    }
+}
---
+++ VoyagerProducts/Interfacce/IPos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoyagerProducts.Interfacce
+{
+    public interface IPos
+    {
+        void SetPricing();
+        void ScanProduct(string prodCode);
+        decimal CalculateTotal();
+    }
+}
